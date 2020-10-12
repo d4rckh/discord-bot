5ef9c8933d3f182e923cc4df8595ef2c3407afbb
Eddy <bishclapper69420@gmail.com>
2020-10-12 07:59:14 +0300

    added chatlogs, added config.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import random
 import os
 from discord.ext import commands
+from config/config.py import discord
 
 client = commands.Bot(command_prefix='!')
 client.remove_command('help')
@@ -26,4 +27,22 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run("")
+#chat logs
+@client.event
+async def on_message(message):
+    with io.open("chatlogs.txt", "a", encoding="utf-8") as f:
+        f.write(
+            "[{}] | [{}] | [{}] @ {}: {}\n".format(message.guild,
+                                                   message.channel,
+                                                   message.author,
+                                                   message.created_at,
+                                                   message.content))
+    f.close()
+    print(
+        Fore.WHITE + "[" + Fore.LIGHTRED_EX + '+' + Fore.WHITE + "]"
+        + Fore.LIGHTRED_EX + "[{}] | [{}] | [{}] @ {}: {}".format(
+            message.guild, message.channel, message.author,
+            message.created_at, message.content))
+
+    await client.process_commands(message)
+client.run(token)
