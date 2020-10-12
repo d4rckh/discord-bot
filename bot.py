029f9dc85818d45d7a3f1cf871055337c6bc9950
Eddy <bishclapper69420@gmail.com>
2020-10-12 10:29:04 +0300

    added admin commands

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import random
 import os
 from discord.ext import commands
-from config/config.py import discord
+from config/config.py import *
 
 client = commands.Bot(command_prefix='!')
 client.remove_command('help')
@@ -45,4 +45,98 @@ async def on_message(message):
             message.created_at, message.content))
 
     await client.process_commands(message)
+#purge command
+@client.command(pass_context=True)
+async def purge(ctx, amount=5):
+    if ctx.message.author.guild_permissions.administrator or ctx.message.author.guild_permissions.ban_members:
+        await ctx.channel.purge(limit=amount)
+        await ctx.author.send(f"purged {amount} messages.")
+        channel = client.get_channel(general_actions_log_channel_id)
+        await channel.send(str(ctx.message.author)+ " " + f"just purged {amount} messages! ")
+
+@client.command(pass_context=True)
+async def warn(ctx, member: discord.Member, *, reason=None):
+    admin = ctx.message.author.guild_permissions.administrator
+    banperm = ctx.message.author.guild_permissions.ban_members
+    if reason == None:
+        await ctx.send("you must enter a reason to warn")
+    else:
+        try:
+            if admin or banperm:
+                message = f"You have been warned from `` {ctx.guild.name} ``\
+                    by `` {ctx.message.author} `` for `` {reason} ``"
+                embed = discord.Embed(
+                    colour=discord.Color.red()
+                )
+                embed.add_field(name="WARNED",
+                                value=message,
+                                inline=False)
+                await member.send(embed=embed)
+                embed = discord.Embed(title=
+                                      "User was warned for {}".format(reason),
+                                      description="**{}** has been warned!"
+                                      .format(member),
+                                      color=discord.Color.green())
+                embed.set_author(name=ctx.message.author,
+                                 icon_url=ctx.message.author.avatar_url)
+
+                await ctx.send(embed=embed)
+                author = ctx.message.author
+                channel = client.get_channel(admin_actions_log_channel_id)
+                await channel.send(f"{author} just warned {member} for {reason} ")
+            else:
+                embed = discord.Embed(title="Permission Denied.",
+                                      description="You don't have \
+                                      permission to use this command.",
+                                      color=discord.Color.red())
+                await ctx.send(embed=embed)
+        except:
+            embed = discord.Embed(title="Permission Denied.",
+                                  description="Bot doesn't have correct \
+                                  permissions, or bot can't ban this user.",
+                                  color=discord.Color.red())
+            await ctx.send(embed=embed)
+
+
+
+
+@client.command(pass_context=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    admin = ctx.message.author.guild_permissions.administrator
+    banperm = ctx.message.author.guild_permissions.ban_members
+
+    if reason == None:
+        await ctx.send("you must enter a reason to ban.")
+    else:
+        try:
+            if admin or banperm:
+                message = f"You have been banned from `` {ctx.guild.name} `` by `` {ctx.message.author} `` for `` {reason}``"
+                embed = discord.Embed(
+                    colour=discord.Color.red()
+                )
+                embed.add_field(name="BANNED",
+                                value=message,
+                                inline=False)
+                await member.send(embed=embed)
+                await ctx.guild.ban(member)
+                embed = discord.Embed(title="User banned was banned for {}".format(reason),
+                                      description="**{}** has been banned!".format(member),
+                                      color=discord.Color.green())
+                embed.set_author(name=ctx.message.author, icon_url=ctx.message.author.avatar_url)
+
+                await ctx.send(embed=embed)
+                author = ctx.message.author
+                channel = client.get_channel(admin_actions_log_channel_id)
+                await channel.send(f"{author} just banned {member} for {reason}")
+            else:
+                embed = discord.Embed(title="Permission Denied.",
+                                      description="You don't have permission to use this command.",
+                                      color=discord.Color.red())
+                await ctx.send(embed=embed)
+        except:
+            embed = discord.Embed(title="Permission Denied.",
+                                  description="Bot doesn't have correct permissions, or bot can't ban this user.",
+                                  color=discord.Color.red())
+            await ctx.send(embed=embed)
+
 client.run(token)
--- config/config.py
+++ config/config.py
@@ -1,3 +1,12 @@
-import string
-
 token = ""
+# the id of the chat to send bot logs of admin value i.e
+# (kick, ban, purge, etc)
+admin_actions_log_channel_id = 741923499542839330
+# the id of the chat to send bot logs of like general commands that were
+# executed
+general_actions_log_channel_id = 741923499542839330
+# the name of the role the user should get once theyve been verified
+verify_role_name = 'Verified'
+# the prefix to be used
+conf_bot_prefix = "+"
+mute_role_name = 'muted'
